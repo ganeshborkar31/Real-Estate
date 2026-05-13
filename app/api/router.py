9d501b312eb9fbeee4551149843753e137e2a922
Ganesh Borkar <ganeshborkar107@gmail.com>
2026-05-13 14:13:21 +0530

    Fix; api tag

--- app/api/router.py
+++ app/api/router.py
@@ -5,5 +5,5 @@ from app.api.v1.router import router as v1_router
 
 
 router = APIRouter()
-router.include_router(health_router, prefix="/api/v1/health", tags=["Health"])
+router.include_router(health_router, prefix="/api/v1/health", tags=["HEALTH"])
 router.include_router(v1_router, prefix="/api/v1")
